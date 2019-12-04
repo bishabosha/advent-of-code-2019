2bda6a5884e17f7713cfe56bc8f437b86650955f
bishabosha <bishbashboshjt@gmail.com>
2019-12-04 04:30:25 +0100

    day 3_2

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -4,31 +4,35 @@ import zio._
 object Day3 with
 
   type Coord = (Int,Int)
-  type Path  = Array[Coord]
+  type Path  = Array[(Int, Coord)]
 
   val Op = raw"([R|L|U|D])(\d+)".r
 
-  def offset(old: Coord)(dir: Char, dist: Int): (Coord, Path) =
+  def offset(old: Coord, current: Int)(dir: Char, dist: Int): (Coord, Int, Path) =
     val x -> y = old
     dir match
-    case 'U' => (x -> (y + dist), (1 to dist).toArray.map(i => x -> (y + i)))
-    case 'D' => (x -> (y - dist), (1 to dist).toArray.map(i => x -> (y - i)))
-    case 'L' => ((x - dist) -> y, (1 to dist).toArray.map(i => (x - i) -> y))
-    case 'R' => ((x + dist) -> y, (1 to dist).toArray.map(i => (x + i) -> y))
+    case 'U' => (x -> (y + dist), current + dist, (1 to dist).toArray.map(i => (current + i, x -> (y + i))))
+    case 'D' => (x -> (y - dist), current + dist, (1 to dist).toArray.map(i => (current + i, x -> (y - i))))
+    case 'L' => ((x - dist) -> y, current + dist, (1 to dist).toArray.map(i => (current + i, (x - i) -> y)))
+    case 'R' => ((x + dist) -> y, current + dist, (1 to dist).toArray.map(i => (current + i, (x + i) -> y)))
 
-  def state(old: Coord)(in: String): Either[String, (Coord, Path)] = in match
-    case Op(op, dist) => Right(offset(old)(op.head, dist.toInt))
+  def state(old: Coord, current: Int)(in: String): Either[String, (Coord, Int, Path)] = in match
+    case Op(op, dist) => Right(offset(old, current)(op.head, dist.toInt))
     case _            => Left(s"malformatted input $in")
 
   def parse(xs: List[String]) =
-    ZIO.foldLeft(xs)(((0,0), Array.empty[Coord])::Nil)((acc, s) =>
-      ZIO.fromEither(state(acc.head._1)(s))
+    ZIO.foldLeft(xs)(((0,0), 0, Array.empty[(Int, Coord)])::Nil)({ (acc, s) =>
+      val (old, current, _) :: rest = acc
+      ZIO.fromEither(state(old, current)(s))
          .map(_ :: acc)
-         .mapError(IllegalArgumentException(_)))
+         .mapError(IllegalArgumentException(_))
+    })
 
-  def parsePath(xs: List[String]) = parse(xs).map(_.map(_._2).toArray.flatten)
+  def parsePath(xs: List[String]) = parse(xs).map(_.map(_._3).toArray.flatMap(_.map(_._2)))
+  def parsePathWithSteps(xs: List[String]) = parse(xs).map(_.map(_._3).toArray.flatten)
 
   val getCoords = inputLines(2) >>= (ZIO.foreachPar(_)(parsePath `compose` (_.split(',').toList)))
+  val getCoordsWithSteps = inputLines(2) >>= (ZIO.foreachPar(_)(parsePathWithSteps `compose` (_.split(',').toList)))
 
   val shortestDist =
     for
@@ -38,6 +42,17 @@ object Day3 with
     yield
       shortest
 
+  val shortestDistWithSteps =
+    for
+      coordss    <- getCoordsWithSteps
+      stepCounts =  coordss.map(_.groupBy(_._2).view.mapValues(_.map(_._1).sortWith(_ < _).head))
+      crosses    =  coordss.map(_.map(_._2).toSet).reduce(_ `intersect` _) - ((0,0))
+      distances  =  crosses.map(xy => stepCounts.map(_(xy)).sum).toList.sortWith(_ < _)
+      shortest   <- ZIO.effect(distances.head)
+    yield
+      shortest
+
   val day3_1 = intChallenge(in="day3",out="day3_1")(shortestDist)
+  val day3_2 = intChallenge(in="day3",out="day3_2")(shortestDistWithSteps)
 
 end Day3
