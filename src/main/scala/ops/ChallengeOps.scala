3bac70db88ddf475d4b3f9d16f279c8519a3a257
bishabosha <bishbashboshjt@gmail.com>
2019-12-08 20:44:55 +0100

    day 8

--- src/main/scala/ops/ChallengeOps.scala
+++ src/main/scala/ops/ChallengeOps.scala
@@ -7,17 +7,17 @@ import zio.console._
 object ChallengeOps with
 
   def challenge[A](in: String)(challenge: TaskR[List[String], A]): URIO[Blocking, Either[String, A]] =
-    (FileIO.lines(s"inputs/$in") >>= challenge.provide).mapError(pprintThrowable).either
+    (FileIO lines s"inputs/$in" >>= challenge.provide) mapError pprintThrowable either
 
   def inputLinesN(n: Int): ZIO[List[String], IndexOutOfBoundsException, List[String]] =
-    ZIO.accessM(xs => ZIO.effect(xs.take(n)).refineToOrDie)
+    ZIO.accessM(xs => ZIO.effect(xs take n) refineToOrDie)
 
   val inputLine: ZIO[List[String], IndexOutOfBoundsException, String] =
-    inputLinesN(1).map(_.head)
+    inputLinesN(1) map (_.head)
 
   val inputInts: ZIO[List[String], NumberFormatException, List[Int]] =
     ZIO.accessM(in =>
       ZIO.traverse(in.zipWithIndex)((s, i) =>
-        StringIO.parseInt(s).mapError(err => NumberFormatException(s"At input ${i + 1}: ${err.getMessage}"))))
+        StringIO parseInt(s) mapError (err => NumberFormatException(s"At input ${i + 1}: ${err.getMessage}"))))
 
   private def pprintThrowable(err: Throwable) = s"${err.getClass.getSimpleName}: ${err.getMessage}"
