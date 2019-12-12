dc236a14dc5e8b5530a08e5996096eee4c9dc772
bishabosha <bishbashboshjt@gmail.com>
2019-12-12 01:36:33 +0100

    specialised IArray.updated

--- src/main/scala/aoc/package.scala
+++ src/main/scala/aoc/package.scala
@@ -1,7 +1,6 @@
 package aoc
 
 import _root_.ops._
-import _root_.ops._
 import reflect.ClassTag
 
 export ops.Challenge, ChallengeOps._, ConsoleOps._, NumericOps.{_, given}, FileIO._, StringIO._
@@ -19,8 +18,8 @@ inline def (a: Int) th = a-1
 
 inline def [A, B, C, D](pair: (A, B)) bimap (f: => A => C, g: => B => D): (C, D) = (f(pair._1), g(pair._2))
 
-def [A: ClassTag](arr: IArray[A]) updated(idx: Int, a: A): IArray[A] =
-  val arr1 = new Array[A](arr.length)
+def (arr: IArray[Long]) updated(idx: Int, a: Long): IArray[Long] =
+  val arr1 = new Array[Long](arr.length)
   System.arraycopy(arr, 0, arr1, 0, arr.length)
   arr1(idx) = a
-  arr1.asInstanceOf[IArray[A]]
+  arr1.asInstanceOf[IArray[Long]]
